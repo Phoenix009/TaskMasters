7e244f64546e15ae422022263d149374cdfaa752
Phoenix009 <jaideepmore307@gmail.com>
2019-12-23 11:47:41 +0530

    added express and body-parser

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 //Root Route
 app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/views/index.html");
 });
 
+
+app.post("/", (req, res)=>{
+    var email = req.body.email;
+    var pass = req.body.pass;
+    
+});
 
 
 app.listen(3000, ()=>{
