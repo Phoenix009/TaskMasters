9541832784b4adcbbdbb13477559f59f32d5feea
Keshav Mishra <mishrakeshav@users.noreply.github.com>
2019-12-27 22:41:15 +0530

    Update stock UI

--- public/js/stocks.js
+++ public/js/stocks.js
@@ -1,15 +1,20 @@
 var btn = $("#additembtn");
 var form = $("#additemform");
 form.hide();
-// document.getElementById("save_button"+'1').style.display="none";
+var savebtn = document.getElementsByClassNameAll("save_button");
+
+console.log(savebtn);
+
+// document.getElementById("save_button").style.display="none";
 btn.on("click", ()=>{
     btn.slideOut();
     form.slideIn();
-})
+});
+
 function edit_row(no)
 {
  document.getElementById("edit_button"+no).style.display="none";
-//  document.getElementById("save_button"+no).style.display="block";
+ document.getElementById("save_button"+no).style.display="block";
 
  var id =document.getElementById(+no);
  var item=document.getElementById("item"+no);
@@ -23,6 +28,6 @@ function edit_row(no)
 
  id.innerHTML=`<input type='text' name='id'  value ='${id_data}' readonly>`;
  item.innerHTML=`<input type='text' name='item_text'  value ='${item_data}' >`;
- avail.innerHTML=`<input type='text' name='avail_text' value ='${avail_data}' >`;
- qty_req.innerHTML=`<input type='text' name='qty_text' value ='${Qty_data}' >`;
+ avail.innerHTML=`<input type='text' name='avail_text'  value ='${avail_data}' >`;
+ qty_req.innerHTML=`<input type='text' name='qty_text'  value ='${Qty_data}' >`;
 }
